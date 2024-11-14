1eb36b27593a7dedf327af2294a564dd33374a5e
Eduardo Oliveira <oliveiraed@ryanair.com>
2024-11-14 09:48:59 +0100

    new user intercept

--- cypress/e2e/users/users.spec.cy.js
+++ cypress/e2e/users/users.spec.cy.js
@@ -3,45 +3,36 @@ describe("Open user ticket app", () => {
     cy.loginViaUi();
   });
 
-  it("Create a new user", () => {
+  it("Create a new user, interceped response", () => {
     cy.visit("/users");
     cy.get("#New_user").click();
     cy.location("pathname").should("include", "users/new");
 
     // fill the form
     cy.get('[placeholder="Name.."]').type("Eduardo");
-    cy.get('[placeholder="Username.."]').type("Eduardo");
-    cy.get('[placeholder="Password.."]').type("Eduardo");
+    cy.get('[placeholder="Username.."]').type("Edu");
+    cy.get('[placeholder="Password.."]').type("123");
     cy.get("select").select("TECH", { force: true });
 
-    // cy.intercept('POST', '/api/users/').as('newUser')
+    cy.intercept(
+      {
+        method: "POST",
+        url: "**/api/users",
+      },
+      {
+        statusCode: 201,
+      }
+    ).as("newUser");
 
-    cy.intercept({
-      method: "POST",
-      url: "*",
-    }).as("newUser");
-
-    // we have code that gets a comment when
-    // the button is clicked in scripts.js
     cy.get('[type="submit"]').click();
 
-    cy.wait("@newUser").then((interception) => {
-      console.log(interception); // first api call
+    cy.wait("@newUser").then(({ request }) => {
+      expect(request && request.body).to.have.property('name', 'Eduardo')
+      expect(request && request.body).to.have.property('username', 'Edu')
+      expect(request && request.body).to.have.property('password', '123')
+      expect(request && request.body).to.have.property('role', 'TECH')
     });
-
-    // cy.wait("@newUser").should(({ request, response }) => {
-    //   console.log(request.body);
-    //   expect(request.headers).to.have.property("content-type");
-    //   expect(response && response.body).to.have.property(
-    //     "name",
-    //     "Using POST in cy.intercept()"
-    //   );
-    // });
-
-    //   cy.wait('@newUser').then((interception) => {
-    //     cy.log(interception) // first api call
-    // })
-
+    cy.wait(200);
     cy.screenshot();
   });
 });
